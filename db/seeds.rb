413b1d42010e3818ab71097f71a80328d4ac8f80
Janett Gerrard <mikanchik@gmail.com>
2023-02-21 16:32:50 +0000

    implemented index and show methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Article.create(title: Faker::Books::Lovecraft.tome, content: Faker::Books::Lovecraft.paragraphs(number: 2))
+end
